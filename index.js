c9ea6046fe825dd864fb2a6cf6e4b19bcfd09659
JOGINDER571 <snshrma7@gmail.com>
2024-07-07 13:01:16 +0530

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const referralRoutes = require('./routes/referralRoutes');
 app.use(express.json());
 app.use('/api/referrals', referralRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
