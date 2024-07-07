31d6d9e351e259b5916311e798869f9a66616943
JOGINDER571 <snshrma7@gmail.com>
2024-07-07 16:39:32 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const  express =require("express");
 const app = express();
 const referralRoutes = require('./routes/referralRoutes');
-
+const cors=require("cors");
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/api/referrals', referralRoutes);
 
